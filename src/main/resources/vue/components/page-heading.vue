08864bca55033602064af4a2ded614155e352b58
mistyfun <softsunshine0912@gmail.com>
2023-09-18 22:18:04 +0800

    Made some adjustments to the card style

---
+++ src/main/resources/vue/components/page-heading.vue
@@ -0,0 +1,10 @@
+<template id="page-heading">
+  <h1 class="text-center mb-4"><slot></slot></h1>
+</template>
+
+<script>
+Vue.component("page-heading", {
+  template: "#page-heading",
+});
+</script>
+<style></style>
---
+++ src/main/resources/vue/components/quest-card.vue
@@ -0,0 +1,39 @@
+<template id="quest-card">
+  <div class="center-container">
+    <v-card width="300px">
+      <v-img :src="quest.imageUrl" alt="Image" height="100px"></v-img>
+      <v-card-title class="py-1">{{ quest.title }}</v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn small @click="show = !show">
+          Hint
+          <v-icon v-if="show">mdi-chevron-up</v-icon>
+          <v-icon v-if="!show">mdi-chevron-down</v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+          <v-card-text>{{ quest.description }} </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </div>
+</template>
+<style>
+.center-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* height: 100vh; Optional: Centers vertically within the viewport */
+}
+</style>
+<script>
+Vue.component("quest-card", {
+  props: ["quest"],
+  data: () => ({
+    show: false,
+  }),
+  template: "#quest-card",
+});
+</script>
