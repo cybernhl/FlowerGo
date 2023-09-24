6710cde656d98e50eca7320175692c3f84b19cc0
mistyfun <softsunshine0912@gmail.com>
2023-09-24 21:16:52 +0800

    Add a button for plant

--- src/main/resources/vue/pages/plant-page.vue
+++ src/main/resources/vue/pages/plant-page.vue
@@ -1,6 +1,8 @@
 <template id="plant-page">
   <app-frame>
     <page-heading>Plant</page-heading>
+    <!-- Button to be placed in the footer -->
+    <v-btn color="primary">Plant</v-btn>
   </app-frame>
 </template>
 <script>
