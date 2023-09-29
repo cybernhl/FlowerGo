f148e6793beccc3735171445d1436ee96cc03029
mistyfun <softsunshine0912@gmail.com>
2023-09-29 23:30:21 +0800

    Add UserController

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -1,5 +1,14 @@
 package user;
 
-public class UserController {
+import io.javalin.http.Context;
 
+public class UserController {
+ private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    public void getUser(Context ctx){
+        ctx.json(userService.getUser(ctx.pathParam("userID")));
+    }
 }
