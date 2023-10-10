2d028c1ac8ae6a8ce8a1b6d525511834c55278d7
mistyfun <softsunshine0912@gmail.com>
2023-10-10 23:36:50 +0800

    Add google Geo API

--- src/main/java/app/flowergo/location/Location.java
+++ src/main/java/app/flowergo/location/Location.java
@@ -1,4 +1,26 @@
 package app.flowergo.location;
 
-public class Location {
-}
+
+    public class Location {
+        public static void main(String[] args) {
+            // 替換成您自己的Google Maps API金鑰
+            String apiKey = "YOUR_API_KEY_HERE";
+
+//            // 創建一個GeoApiContext對象
+//            GeoApiContext context = new GeoApiContext.Builder()
+//                    .apiKey(apiKey)
+//                    .build();
+//
+//            try {
+//                // 進行地理編碼查詢
+//                GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway, Mountain View, CA").await();
+//
+//                // 輸出結果
+//                System.out.println(results[0].formattedAddress);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+
